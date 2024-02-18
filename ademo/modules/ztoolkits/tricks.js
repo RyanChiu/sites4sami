07e5040828d73b37512e823c00c93ff8fbe2c443
Ryan Chiu <ryan.chiu@hotmail.com>
2024-02-18 16:19:24 +0800

    use async/await to implement the queries, and seal the methods in tricks.js

--- ademo/modules/ztoolkits/tricks.js
+++ ademo/modules/ztoolkits/tricks.js
@@ -1,3 +1,43 @@
+const net = require('net');
+const mysql = require('mysql2');
+
+/* connection pool */
+const pool = mysql.createPool({
+    host: "localhost",
+    user: "sami",
+    password: "4sites0fsami",
+    database: "gwr",
+    //stream: net.connect('/var/run/mysqld/mysqld.sock')
+    stream: function() {
+        var socket = net.connect('/var/run/mysqld/mysqld.sock');
+        socket.setKeepAlive(true);
+        return socket;
+    }
+})
+
+/* promised query */
+exports.queryPromise = function(sql) {
+    return new Promise((resolve, reject) => {
+       pool.query(sql, (error, results) => {
+            if (error) {
+                return reject(error);
+            }
+            return resolve(results);
+       })
+    })
+}
+
+/* query */
+exports.queryData = async function (sql) {
+    try {
+        const data = await queryPromise(sql);
+        console.log(data);
+        return data;
+    } catch(error) {
+        console.log(error);
+    }
+}
+
 exports.getTitle = function (t) {
     t = t.split('\\').pop().split('/').pop();
     t = t.substring(0, t.indexOf("."));
--- ademo/routes/agents.js
+++ ademo/routes/agents.js
@@ -1,32 +1,21 @@
 var express = require('express');
 var router = express.Router();
-const net = require('net');
-const mysql = require('mysql2');
-
-/* GET agents listing. */
-var con = mysql.createConnection({
-	host: "localhost",
-	user: "sami",
-	password: "4sites0fsami",
-	database: "gwr",
-	stream: net.connect('/var/run/mysqld/mysqld.sock')
-});
-
-con.connect(function(err) {
-	if (err) throw err;
-	console.log("Connected!");
-});
+const tricks = require('../modules/ztoolkits/tricks');
 
 /* render the page */
 router.get('/', function(req, res, next) {
   //res.send('respond with a resource');
-  var data = null;
-  con.query("select * from user", function(err, result) {
-    if (err) throw err;
-    data = result;
-    console.log(result);
-    res.render('agents', { title: 'Agents', data: data });
-  });
+  async function renderIt(sql) {
+    try {
+        const data = await tricks.queryPromise(sql);
+        res.render('agents', { title: 'Agents', data: data });
+        console.log(data);
+    } catch(error) {
+        console.log(error);
+    }
+  }
+
+  renderIt("select * from user");
 
   //res.render('agents', { title: 'Agents', data: data });
   
