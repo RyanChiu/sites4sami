eaee02bd2aeb86b62ecaaf803378a7835d488c38
Ryan Chiu <ryan.chiu@hotmail.com>
2024-02-28 10:16:57 +0800

    fix the "view_agent" with sql that missing the new agent not logged in.

--- ademo/misc/dbs/view_agent.sql
+++ ademo/misc/dbs/view_agent.sql
@@ -3,3 +3,7 @@ select a.id, a.username, a.1stname, a.lstname, a.password, a.note, b.username as
 from user a, (select id, username from user where type = 2) b, log c
 where a.id = c.userid and a.officeid = b.id and a.type = 3 and c.type = 1
 group by a.id
+union
+select a.id, a.username, a.1stname, a.lstname, a.password, a.note, b.username as office, a.status, a.registered, null as lastlogintime, 0 as logintimes
+from user a, (select id, username from user where type =2) b
+where a.id not in (select distinct userid from log where type = 1) and a.officeid = b.id and a.type = 3
