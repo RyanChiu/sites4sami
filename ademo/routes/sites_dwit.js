2d51e4d5bc362a956c0a16c97f22eece58368a9d
Ryan Chiu <ryan.chiu@hotmail.com>
2024-03-10 17:08:52 +0800

    keep fix the last bug.

--- ademo/routes/sites_dwit.js
+++ ademo/routes/sites_dwit.js
@@ -7,7 +7,7 @@ tricks.useSession(router);
 /* render the page */
 router.get('/', async function(req, res, next) {
   if (req.session && req.session.loggedin
-      && (req.session.role == 0 || req.session.role.role == 1)) {
+      && (req.session.role == 0 || req.session.role == 1)) {
     var params = req.query, paramOp = "", paramId = "";
     if (JSON.stringify(params) == '{}'
         || typeof(params["op"]) == "undefined" 
