aa9921b19f521df08c86847aa31bb295158a4b69
Ryan Chiu <ryan.chiu@hotmail.com>
2024-03-24 17:19:55 +0800

    make captcha much more easier when doing the math, and make the background more sightful.

--- ademo/routes/captcha.js
+++ ademo/routes/captcha.js
@@ -8,10 +8,11 @@ var svgCaptcha = require('svg-captcha');
 /* GET captcha. */
 router.get('/', function(req, res, next) {
     var captcha = svgCaptcha.createMathExpr( {
-        noise: 2,
+        noise: 1,
         color: true,
+        background: '#333',
         mathMin: 2,
-        mathMax: 41
+        mathMax: 21
     });
     req.session.captcha = captcha.text;
     //console.log("[z.debug.login]"); console.log(captcha.data);
