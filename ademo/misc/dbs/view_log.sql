03ef088b261ee55c8d88d15d82ff97867fbe3cc4
Ryan Chiu <ryan.chiu@hotmail.com>
2024-04-06 00:07:49 +0800

    show ip in login logs.

--- ademo/misc/dbs/view_log.sql
+++ ademo/misc/dbs/view_log.sql
@@ -1,2 +1,8 @@
-create view view_log as select a.id, a.userid, b.username, b.type as role, (select username from user where id = b.officeid) as office, intime, outtime from log
- a, user b where a.userid = b.id;
+drop view view_log;
+create view view_log as
+select 
+	`a`.`id` AS `id`,`a`.`userid` AS `userid`,`b`.`username` AS `username`,`b`.`type` AS `role`,
+	(select `user`.`username` from `user` where (`user`.`id` = `b`.`officeid`)) AS `office`,
+	`a`.`ip4` AS `ip4`,`a`.`intime` AS `intime`,`a`.`outtime` AS `outtime` 
+from (`log` `a` join `user` `b`) 
+where (`a`.`userid` = `b`.`id`);
