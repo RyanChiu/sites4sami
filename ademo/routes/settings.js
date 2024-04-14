fbe9ae5bd10cae0658b8c837b92ca774d15bb8f6
Ryan Chiu <ryan.chiu@hotmail.com>
2024-04-14 22:34:51 +0800

    implement "rich countries" setting for superadin and only for superadmin. (but not connect to the nav2, it's still hard codes there in nav2)

--- ademo/routes/settings.js
+++ ademo/routes/settings.js
@@ -5,14 +5,40 @@ const tricks = require('../modules/ztoolkits/tricks');
 tricks.useSession(router);
 
 /* render the page */
-router.get('/', function(req, res, next) {
-  if (req.session && req.session.loggedin) {
+router.get('/', async function(req, res, next) {
+  if (req.session && req.session.loggedin && req.session.role == 0) {
     var title = tricks.getTitle(__filename);
-    res.render('home', { 
+    let countries = await tricks.queryData("select * from country");
+    var richOnes = [];
+    for (let c of countries) {
+      if (c["rich"] == 1) {
+        richOnes.push(c["isoCode"]);
+      }
+    }
+    res.render('settings', { 
       title: title,
       navs: req.session.navs,
-      user: req.session.username
+      user: req.session.username,
+      countries: countries,
+      richOnes: richOnes
     });
+  } else {
+    res.redirect('logout');
+  }
+});
+
+/* deal with post data */
+router.post('/', async (req, res, next) => {
+  if (req.session && req.session.loggedin && req.session.role == 0) {
+    var rst;
+    if (req.body.iptCountries !== undefined) {
+      await tricks.queryData("update country set rich = 0");
+      rst = await tricks.queryData("update country set rich = 1 where isoCode in (?) ", [req.body.iptCountries]);
+    } else {
+      rst = await tricks.queryData("update country set rich = 0");
+    }
+    //console.log(`[debug from settings:]${JSON.stringify(rst)}`);
+    res.redirect('settings');
   } else {
     res.redirect('logout');
   }
