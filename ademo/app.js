615c99fe2eebb4585a1ee512f344ef28143b0a40
Ryan Chiu <ryan.chiu@hotmail.com>
2024-10-20 15:57:28 +0800

    only tips on home page instead of showing "debug" infos on an error page.

--- ademo/app.js
+++ ademo/app.js
@@ -78,7 +78,8 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  // res.render('error');
+  res.redirect('home?tips=Something went wrong.....');
 });
 
 module.exports = app;
