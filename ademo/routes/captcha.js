773ed93407c6dc5a9656b39f5b771d1efa925869
Ryan Chiu <ryan.chiu@hotmail.com>
2024-10-21 19:50:04 +0800

    make captcha more human-readable by putting the background white and foreground text blue.

--- ademo/routes/captcha.js
+++ ademo/routes/captcha.js
@@ -8,11 +8,13 @@ var svgCaptcha = require('svg-captcha');
 /* GET captcha. */
 router.get('/', function(req, res, next) {
     var captcha = svgCaptcha.createMathExpr( {
-        noise: 1,
+        noise: 3,
         color: true,
+        background: "white",
         mathMin: 2,
         mathMax: 21
     });
+    captcha.data = captcha.data.replace(/fill="#[A-Fa-f0-9]{6}"/g, 'fill="#0000FF"');
     req.session.captcha = captcha.text;
     //console.log("[z.debug.login]"); console.log(captcha.data);
     res.type('svg');
