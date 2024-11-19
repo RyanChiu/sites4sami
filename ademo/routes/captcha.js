af51dd5c6c543803201d7935a27024fd79399d6f
Ryan Chiu <ryan.chiu@hotmail.com>
2024-11-19 20:26:23 +0800

    add "Arial Rounded Bold" font and load it for svg-captcha

--- ademo/routes/captcha.js
+++ ademo/routes/captcha.js
@@ -4,17 +4,19 @@ const tricks = require('../modules/ztoolkits/tricks');
 
 tricks.useSession(router);
 var svgCaptcha = require('svg-captcha');
+svgCaptcha.loadFont(__dirname + '/../public/webfonts/ArialRoundedBold.ttf');
 
 /* GET captcha. */
 router.get('/', function(req, res, next) {
     var captcha = svgCaptcha.createMathExpr( {
         noise: 3,
-        color: true,
+        color: false,
+        fontSize: 60,
         background: "white",
         mathMin: 2,
         mathMax: 21
     });
-    captcha.data = captcha.data.replace(/fill="#[A-Fa-f0-9]{6}"/g, 'fill="#0000FF"');
+    captcha.data = captcha.data.replace(/fill="#[A-Fa-f0-9]{6}"/g, 'fill="black" stroke="black" stroke-width="1"');
     req.session.captcha = captcha.text;
     //console.log("[z.debug.login]"); console.log(captcha.data);
     res.type('svg');
