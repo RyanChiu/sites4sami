73c2182f134b6594aab28accf16007d0fa3cd42b
Ryan Chiu <ryan.chiu@hotmail.com>
2024-11-25 09:54:49 +0800

    change the "create talbe sales" into the final form

--- ademo/misc/dbs/sales.sql
+++ ademo/misc/dbs/sales.sql
@@ -1,16 +1,16 @@
 CREATE TABLE `sales` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `day_pulling` date not null comment 'like 2024-11-21, data of which request by pulling from server',
-  `trxid` varchar(64) not null comment 'transaction_id',
-  `trxtime` int comment 'click_unix_timestamp, a 10-digit num',
-  `agent` varchar(32) NOT NULL comment 'not id of the agent, but user name of it',
-  `link_abbr` varchar(8) NOT NULL comment 'not the id of a type, but the abbreviation of it',
+  `day_pulling` date NOT NULL COMMENT 'like 2024-11-21, data of which request by pulling from server',
+  `trxid` varchar(64) NOT NULL COMMENT 'transaction_id',
+  `trxtime` int DEFAULT NULL COMMENT 'click_unix_timestamp, a 10-digit num',
+  `agent` varchar(32) NOT NULL COMMENT 'not id of the agent, but user name of it',
+  `type_abbr` varchar(8) NOT NULL COMMENT 'not id of the site, but abbreviation of it',
   `conversion_id` varchar(64) NOT NULL,
-  `conversion_user_ip` varchar(16),
-  `country` varchar(64),
-  `region` varchar(32),
-  `city` varchar(32),
-  `referer` varchar(64),
-  PRIMARY KEY (`id`)
-);
-create unique index transactionid on sales(trxid);
+  `conversion_user_ip` varchar(16) DEFAULT NULL,
+  `country` varchar(64) DEFAULT NULL,
+  `region` varchar(32) DEFAULT NULL,
+  `city` varchar(32) DEFAULT NULL,
+  `referer` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `transactionid` (`trxid`)
+)
